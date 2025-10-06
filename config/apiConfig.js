7e0de2147fad71fc91552775dfdf85ec1ed1b05f
hokhoi1979 <166026796+hokhoi1979@users.noreply.github.com>
2025-10-06 20:08:14 +0700

    pull

---
+++ config/apiConfig.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    console.log("👉 Request URL:", config.baseURL + config.url);
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API error:", error.response?.data || error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
